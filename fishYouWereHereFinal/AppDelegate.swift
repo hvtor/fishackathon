28b4466b005809ee67c6ec14ace024f8950fc12e
Hemant Torsekar <hvt@me.com>
2015-06-06 03:53:23 -0400

    Addition of CollectionViewController CTRL and CollectionViewCell Model.

--- fishYouWereHereFinal/AppDelegate.swift
+++ fishYouWereHereFinal/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // CornFlowerBlue #4A86E8
+
         return true
     }
 
--- fishYouWereHereFinal/SecondViewController.swift
+++ fishYouWereHereFinal/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
